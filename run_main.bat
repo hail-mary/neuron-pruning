e2784f1e7db4672a9db569a6c4290eedec0e1e4c
hail_mary <s246215w@st.go.tuat.ac.jp>
2025-01-31 17:28:02 +0900

    Update as of 1/31

--- run_main.bat
+++ run_main.bat
@@ -6,6 +6,7 @@ python main.py --logdir drop_cheetah --env HalfCheetah-v5
 python main.py --logdir drop_hopper --env Hopper-v5
 python main.py --logdir drop_pusher --env Pusher-v5
 python main.py --logdir drop_reacher --env Reacher-v5
+python main.py --logdir drop_swimmer --env Swimmer-v5
 python main.py --logdir drop_walker --env Walker2d-v5
 
 REM Print a message
