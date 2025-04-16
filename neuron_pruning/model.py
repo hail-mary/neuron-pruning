4918a1a9c6fac883c4deca50970dce825b0d8981
hail-mary <s246215w@st.go.tuat.ac.jp>
2025-04-16 12:20:15 +0200

    Update as of 4/16

---
+++ neuron_pruning/model.py
@@ -0,0 +1,84 @@
+import gymnasium as gym
+import stable_baselines3
+import torch
+import os
+import numpy as np
+import cloudpickle
+
+class Model:
+    def __init__(self, cfg):
+        self.cfg = cfg
+        self.env = gym.make(cfg['env_name'], render_mode=cfg['render_mode'])
+        self.make_policy(self.env)
+    
+    @property
+    def policy_kwargs(self):
+        return self.model.policy_kwargs
+    
+    @property
+    def policy(self):
+        return self.model.policy
+    
+    def make_policy(self, env, policy_kwargs=None, policy_weights=None):
+        if policy_kwargs is None: # to initialize
+            policy_kwargs = self.cfg['policy_kwargs']
+
+        if isinstance(policy_kwargs['activation_fn'], str): # load from cfg
+            activation_fn = getattr(torch.nn, policy_kwargs['activation_fn'])
+        else:
+            activation_fn = policy_kwargs['activation_fn']
+
+        policy_kwargs = dict(
+            activation_fn=activation_fn,
+            net_arch=policy_kwargs['net_arch']
+        )
+        algorithm = getattr(stable_baselines3, self.cfg['algorithm'])
+        device = self.cfg['device']
+        # self.model = None
+        self.model = algorithm("MlpPolicy", env, verbose=0, policy_kwargs=policy_kwargs, device=device)
+        if policy_weights is not None:
+            self.model.policy.load_state_dict(policy_weights)
+
+    def save_policy(self, save_to=''):
+        policy_kwargs = self.model.policy_kwargs
+        policy_weights = self.model.policy.state_dict()
+        with open(f'{save_to}/policy_kwargs.pkl', 'wb') as f:
+            cloudpickle.dump(policy_kwargs, f)
+        with open(f'{save_to}/policy_weights.pkl', 'wb') as f:
+            cloudpickle.dump(policy_weights, f)
+
+    def load_policy(self, load_from=''):
+        for file_name in os.listdir(load_from):
+            if 'policy_kwargs.pkl' in file_name:
+                with open(os.path.join(load_from, file_name), 'rb') as f:
+                    deserialized_kwargs = cloudpickle.load(f)
+            if 'policy_weights.pkl' in file_name:
+                with open(os.path.join(load_from, file_name), 'rb') as f:
+                    deserialized_weights = cloudpickle.load(f)
+
+        self.make_policy(self.env, deserialized_kwargs, deserialized_weights)
+
+    def learn(self, total_timesteps):
+        self.model.learn(total_timesteps)
+
+    def evaluate_policy(self, seed=None, num_eval_episodes=1, num_eval_steps_per_episode=1000, record_video=False):
+        total_rewards = []
+        
+        # Wrap the environment for video recording if record_video is True
+        if record_video:
+            video_folder = os.path.join(self.cfg['logdir'], 'videos')
+            self.env = gym.wrappers.RecordVideo(self.env, video_folder=video_folder, episode_trigger=lambda x: True)
+        
+        for _ in range(num_eval_episodes):
+            observation, info = self.env.reset(seed=seed)
+            episode_reward = 0
+            for _ in range(num_eval_steps_per_episode):
+                action, _ = self.model.predict(observation, deterministic=True)
+                observation, reward, terminated, truncated, info = self.env.step(action)
+                episode_reward += reward
+                if terminated or truncated:
+                    self.env.reset(seed=seed)
+            total_rewards.append(episode_reward)
+        
+        self.env.close()
+        return np.mean(total_rewards)
---
+++ neuron_pruning/trainer.py
@@ -0,0 +1,126 @@
+import torch
+import numpy as np
+
+class Trainer:
+    """
+    a set of utility functions for training
+    """
+    def __init__(self, cfg):
+        self.cfg = cfg
+
+    
+    def average_params(self, params_list):
+        avg_params = {}
+        for key in params_list[0].keys():
+            avg_params[key] = torch.stack([params[key] for params in params_list]).mean(dim=0)
+        return avg_params
+    
+    def calculate_norms(self, pre_avg_params, avg_params):
+        pre_avg_norm = torch.cat([p.flatten() for p in pre_avg_params.values()]).norm()
+        avg_norm = torch.cat([p.flatten() for p in avg_params.values()]).norm()
+        return pre_avg_norm, avg_norm
+
+    def preprocess(self, raw_arch, raw_params):
+        # First, preprocess arch
+        layer_types = ('policy', 'value')
+        archs = tuple(raw_arch.values())
+        arch = dict(zip(layer_types, archs))
+
+        # Then, preprocess params
+        params = {}
+        for layer_type, layers in arch.items():
+            for key, val in raw_params.items():
+                if layer_type in key and 'weight' in key:
+                    params.update({key: val})
+                elif layer_type in key and 'bias' in key:
+                    params.update({key: val})
+                elif 'action' in key and layer_type == 'policy':
+                    params.update({key: val})
+
+        # keep auxilary info to reconstruct
+        aux = {'log_std': raw_params.pop('log_std'), 
+               'action_net.bias': raw_params.pop('action_net.bias'),
+               'value_net.bias': raw_params.pop('value_net.bias'),
+        }
+
+        return arch, params, aux
+
+    def reconstruct(self, arch, params, aux):
+        # First, reconstruct arch
+        layer_types = ('pi', 'vf')
+        archs = tuple(arch.values())
+        processed_arch = dict(zip(layer_types, archs))
+
+        # Then, reconstruct params
+        # Missing key(s) in state_dict: "log_std", "action_net.bias", "value_net.bias"
+        processed_params = params
+        processed_params.update(aux)
+
+        return processed_arch, processed_params
+
+    def remove_indices(self, tensor, indices_to_remove, row_or_col=None):
+        if tensor.dim() == 1: # for bias
+            # Create a boolean mask indicating which elements to keep
+            mask = torch.ones(tensor.shape[0], dtype=torch.bool)
+            mask[indices_to_remove] = False
+        elif tensor.dim() == 2 and row_or_col == 'row': # for weight
+            # Create a boolean mask indicating which rows to keep
+            mask = torch.ones_like(tensor, dtype=torch.bool)
+            mask[indices_to_remove, :] = False
+        elif tensor.dim() == 2 and row_or_col == 'col': # for weight
+            # Create a boolean mask indicating which columns to keep
+            mask = torch.ones_like(tensor, dtype=torch.bool)
+            mask[:, indices_to_remove] = False
+        else:
+            raise ValueError("Invalid tensor dimension or row_or_col value")
+
+        # Apply the mask to select the desired elements
+        new_tensor = tensor[mask]
+        if row_or_col == 'row':
+            new_units = tensor.shape[0] - len(indices_to_remove)
+            new_tensor = new_tensor.reshape(new_units, tensor.shape[1])
+        elif row_or_col == 'col':
+            new_units = tensor.shape[1] - len(indices_to_remove)
+            new_tensor = new_tensor.reshape(tensor.shape[0], new_units)
+
+        return new_tensor
+
+    def modify_network(self, params, arch, dropout_rates):
+        for layer_type, layers in arch.items():
+            for layer_idx in range(len(layers)):
+                if dropout_rates[layer_type][layer_idx] > 0.0:
+                    num_to_drop = int(layers[layer_idx] * dropout_rates[layer_type][layer_idx])
+                    if num_to_drop == 0:
+                        continue
+
+                    # First, modify network architecture
+                    arch[layer_type][layer_idx] -= num_to_drop
+
+                    # Then, modify network parameters
+                    # for random elimination:
+                    # indices_to_remove = np.random.choice(layers[layer_idx], num_to_drop, replace=False)
+
+                    # Process weights
+                    weight_key_1 = f"mlp_extractor.{layer_type}_net.{2 * layer_idx}.weight"  # Adjust the key format as per your architecture
+                    if weight_key_1 in params:
+                        weight_magnitude = torch.sqrt(torch.sum(params[weight_key_1] ** 2, dim=1))
+                        values, indices_to_remove = torch.topk(weight_magnitude, num_to_drop, largest=False)
+                        params[weight_key_1] = self.remove_indices(params[weight_key_1], indices_to_remove, row_or_col='row')
+                    
+                    # Process biases
+                    bias_key = f"mlp_extractor.{layer_type}_net.{2 * layer_idx}.bias"  # Adjust the key format as per your architecture
+                    if bias_key in params:
+                        params[bias_key] = self.remove_indices(params[bias_key], indices_to_remove)
+
+                    # Process weights
+                    weight_key_2 = f"mlp_extractor.{layer_type}_net.{2 * (layer_idx + 1)}.weight"  # Adjust the key format as per your architecture
+                    if weight_key_2 in params:
+                        params[weight_key_2] = self.remove_indices(params[weight_key_2], indices_to_remove, row_or_col='col')
+
+                    # Process output
+                    output_key = "action_net.weight" if layer_type == 'policy' else "value_net.weight"
+                    if output_key in params and layer_idx == len(layers) - 1 :
+                        params[output_key] = self.remove_indices(params[output_key], indices_to_remove, row_or_col='col')
+
+        return arch, params
+
